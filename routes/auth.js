1490114414c55e750f44a504e17a83e8dd991969
Ahmadansari1942 <sn0259152@gmail.com>
2026-02-05 15:54:19 +0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,7 @@ router.get('/login', (req, res) => {
 });
 
 module.exports = router;
-const express = require('express');
-const router = express.Router();
 
-router.get('/login', (req, res) => {
-  res.render('login');
-});
 
 router.get('/register', (req, res) => {
   res.render('register');
