0312addc56507a81b14b9b940ad2dd9f07c741dd
Ahmadansari1942 <sn0259152@gmail.com>
2026-02-05 16:05:22 +0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/data', (req, res) => {
-  res.json({ message: "API Working" });
+// Login route
+router.get('/login', (req, res) => {
+  res.send('Login Page'); 
+  // Agar EJS use karna chahte ho to:
+  // res.render('login');
+});
+
+// Register route
+router.get('/register', (req, res) => {
+  res.render('register'); // register.ejs file honi chahiye views folder me
 });
 
 module.exports = router;
+
