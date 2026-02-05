e91db401cd544280385d96c12864fb5445cb1748
Ahmadansari1942 <sn0259152@gmail.com>
2026-02-05 15:16:36 +0500

    Add routes for task creation and editing

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,3 +6,20 @@ router.get('/tasks', (req, res) => {
 });
 
 module.exports = router;
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('tasks'); // tasks.ejs
+});
+
+router.get('/create', (req, res) => {
+  res.render('create-task');
+});
+
+router.get('/edit/:id', (req, res) => {
+  res.render('edit-task', { id: req.params.id });
+});
+
+module.exports = router;
+
